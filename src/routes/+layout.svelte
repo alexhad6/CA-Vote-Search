d40950c072bf21fd82ac4a2f785a116fda10fbfd
Alex Hadley <alexhad6@users.noreply.github.com>
2023-12-16 13:41:03 -0800

    Add generation time on page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { LayoutData } from "./$types";
+	import { SITE_NAME } from "$lib/constants";
+
+	export let data: LayoutData;
+</script>
+
+<svelte:head>
+	<title>{SITE_NAME}</title>
+</svelte:head>
+
+<h1>{SITE_NAME}</h1>
+
+<slot />
+
+<footer>Page generated at {data.currentTime}.</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,1 @@
-<script lang="ts">
-	import { SITE_NAME } from "$lib/constants";
-</script>
-
-<svelte:head>
-	<title>{SITE_NAME}</title>
-</svelte:head>
-
-<h1>{SITE_NAME}</h1>
 <p>Currently under construction.</p>
