f0cabd626ee359b5539f86e9540a4f677f50bb7a
Alex Hadley <alexhad6@users.noreply.github.com>
2023-12-15 23:58:22 -0800

    Add initial Svelte files

---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+/** Name of the site. */
+export const SITE_NAME = "CA Vote Search";
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { SITE_NAME } from "$lib/constants";
+</script>
+
+<svelte:head>
+	<title>{SITE_NAME}</title>
+</svelte:head>
+
+<h1>{SITE_NAME}</h1>
+<p>Currently under construction.</p>
