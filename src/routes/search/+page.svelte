ea906ac344fc2b671f8596bf2230a070ef8067ba
Alex Hadley <alexhad6@users.noreply.github.com>
2023-12-20 11:35:57 -0500

    Remove extra slash before search parameters

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
 	import { page } from "$app/stores";
+	import { SITE_NAME } from "$lib/constants";
+
+	// Remove slash before search parameters
+	const { href, searchParams } = $page.url;
+	const urlWithoutSearchParams = href.split("?")[0];
+	if (urlWithoutSearchParams.slice(-1) === "/") {
+		history.replaceState(
+			history.state,
+			"",
+			`${urlWithoutSearchParams.slice(0, -1)}?${searchParams}`,
+		);
+	}
 </script>
+
+<svelte:head>
+	<title>{$page.url.searchParams.get("bill-number")} - {SITE_NAME}</title>
+</svelte:head>
 
 {#each $page.url.searchParams as [key, value]}
 	<p>{key} is "{value}"</p>
